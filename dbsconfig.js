80aefc60b9437edcac7f7978c7d1183ec00a1059
Sara Gabriela Arteaga <sgarteaga@siesa.com>
2022-11-04 19:14:35 -0500

    Proyecto NodeJs Universidad

---
+++ dbsconfig.js
@@ -0,0 +1,12 @@
+const config = {
+    user:"sa",
+    password:"Sa123456",
+    server: "P553FAC",
+    database: "COLEGIO",
+    options:{
+        trustedConnection: true,
+         trustServerCertificate: true,
+
+    }
+    }
+    module.exports = config
